c7aeec1994eff6d5198f681d460ae73d769dd296
abdkhaleel <khaleelkhadeer.ras@gmail.com>
2024-02-05 15:41:23 +0530

    contactus

---
+++ app/contactus/page.jsx
@@ -0,0 +1,25 @@
+
+'use client';
+
+import { Card } from 'flowbite-react';
+import React from "react";
+export default function Component() {
+  return (
+	<>
+    <Card href="#" className="max-w-sm">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        ENERGY AND FUEL USERS’ ASSOCIATION OF INDIA
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+	  4B-1, JP TOWERS<br/>
+7/2, NUNGAMBAKKAM HIGH ROAD<br/>
+CHENNAI – 600 034.<br/>
+TAMIL NADU. INDIA<br/>
+PHONE : 91-44-28278604<br/>
+FAX : 91-44-28205553<br/>
+		</p>
+    </Card>
+
+	</>
+  );
+}
